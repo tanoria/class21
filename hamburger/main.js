83c9310ecdb3d737c92f6dd981199538e920730e
tanoria <tania.derlis@gmail.com>
2016-03-29 20:39:09 -0400

    initial commit

---
+++ hamburger/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	jQuery Goes here!
+	$('.js-menu').on('click touchstart', 
+		function(){
+		console.log('menu open')
+		$('.mainNav').fadeIn().addClass('showNav');
+	})
+
+	$('.zoomMe').zoom();
+
+});
